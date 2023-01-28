5a5b7680296bfe347a3127fc66a4c7481a8da47f
Olena <newsroommy@gmail.com>
2023-01-29 00:42:00 +0300

    add components

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ImageSearch from './modules/ImageSearch/ImageSearch';
+function App() {
+  return (
+    <div className="App">
+      <ImageSearch />
+    </div>
+  );
+}
+export default App;
